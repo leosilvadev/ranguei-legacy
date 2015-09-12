ddfdcddc29445f391e69af25b26a02d327e596f0
Leonardo JG Silva <leosilvadev@gmail.com>
2015-09-11 21:09:57 -0300

    configuration to run on heroku

--- src/main/groovy/com/ranguei/App.groovy
+++ src/main/groovy/com/ranguei/App.groovy
@@ -1,13 +1,21 @@
 package com.ranguei
 
-import io.github.javaconductor.gserv.delegates.HttpMethodDelegate
+import io.github.javaconductor.gserv.GServ
 
 import com.ranguei.routes.Router
 
 class App {
 	
 	static main(args){
-		new Router().configure()
+		def gserv = new GServ()
+		
+		def resources = Router.configure(gserv)
+		
+		gserv.http {
+			resources.each {
+				resource it
+			}
+		}.start(8080)
 	}
 
 }
--- src/main/groovy/com/ranguei/routes/Router.groovy
+++ src/main/groovy/com/ranguei/routes/Router.groovy
@@ -6,18 +6,11 @@ import com.ranguei.controllers.HelloController
 
 class Router {
 
-	def configure(){
-		def gserv = new GServ()
-		
-		def resourceTest = gserv.resource("/tests", HelloController.actions)
-		def resourceTest2 = gserv.resource("/tests2", HelloController.actions)
-		
-		
-		
-		gserv.http {
-			resource resourceTest
-			resource resourceTest2
-		}.start(8080);
+	static def configure(gserv){
+		def resources = []
+		resources << gserv.resource("/tests", HelloController.actions)
+		resources << gserv.resource("/tests2", HelloController.actions)
+		resources
 	}
 	
 }
