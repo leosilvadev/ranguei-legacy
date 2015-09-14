c897a8aeefb6d5da8e862bdae2e20d8e7df12caf
Leonardo JG Silva <leosilvadev@gmail.com>
2015-09-13 22:27:47 -0300

    fixing gruntbuildTask class

--- buildSrc/src/main/groovy/tasks/GruntBuildTask.groovy
+++ buildSrc/src/main/groovy/tasks/GruntBuildTask.groovy
@@ -3,7 +3,7 @@ package tasks
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
-class BowerInstallTask extends DefaultTask {
+class GruntBuildTask extends DefaultTask {
 	@TaskAction
 	def install() {
 		println './node_modules/grunt-cli/bin/grunt build'.execute().text
