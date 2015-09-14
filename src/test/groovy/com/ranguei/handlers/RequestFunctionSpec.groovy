f2f62943a5202c4c53dff105224a33516a349867
Leonardo JG Silva <leosilvadev@gmail.com>
2015-09-13 22:31:53 -0300

    fixing test

--- src/test/groovy/com/ranguei/handlers/RequestFunctionSpec.groovy
+++ src/test/groovy/com/ranguei/handlers/RequestFunctionSpec.groovy
@@ -3,6 +3,7 @@ package com.ranguei.handlers
 import spock.lang.Specification
 
 import com.ranguei.exceptions.InvalidArgumentException
+import com.ranguei.handlers.request.RequestData;
 import com.ranguei.handlers.request.RequestHandler
 import com.ranguei.handlers.request.RequestSuccessHandler
 
@@ -10,13 +11,13 @@ class RequestFunctionSpec extends Specification {
 
 	def "Should create a success callback function when pass an success callback"(){
 		Closure mockSuccess = Mock(Closure)
-		InputStream mockInStream = Mock(InputStream)
+		RequestData mockData = Mock(RequestData)
 		
 		given: "a function to be executed"
 			Closure function = { true }
 			
 		and: "a request handler function"
-			def requestFunction = new RequestHandler( mockInStream, function )
+			def requestFunction = new RequestHandler( mockData, function )
 			
 		when: "the request function register a success function"
 			def successFn = requestFunction.onSuccess mockSuccess
@@ -26,13 +27,13 @@ class RequestFunctionSpec extends Specification {
 	}
 	
 	def "Should throw an exception with a null success callback"(){
-		InputStream mockInStream = Mock(InputStream)
+		RequestData mockData = Mock(RequestData)
 		
 		given: "a function to be executed"
 			Closure function = { true }
 			
 		and: "a request handler function"
-			def requestFunction = new RequestHandler( mockInStream, function )
+			def requestFunction = new RequestHandler( mockData, function )
 			
 		when: "the request function try to register a null success function"
 			requestFunction.onSuccess null
@@ -42,13 +43,13 @@ class RequestFunctionSpec extends Specification {
 	}
 	
 	def "Should throw an exception with a null function to execute"(){
-		InputStream mockInStream = Mock(InputStream)
+		RequestData mockData = Mock(RequestData)
 		
 		given: "a null function"
 			def function = null
 			
 		when: "the request handler function is created with this null function"
-			def requestFunction = new RequestHandler( mockInStream, function )
+			def requestFunction = new RequestHandler( mockData, function )
 			
 		then: "an InvalidArgumentException must be thrown"
 			thrown(InvalidArgumentException)
