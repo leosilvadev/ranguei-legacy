d9d1aceb645cc8866822a36784f555d1e18bb6f7
Leonardo JG Silva <leosilvadev@gmail.com>
2015-09-14 21:52:41 -0300

    removing dist files

--- dist/js/modules/guest/services/services.js
+++
@@ -1,7 +0,0 @@
-(function(){
-	
-	'use strict';
-	
-	angular.module('ranguei.guest.services', []);
-	
-})();
--- dist/js/rangueiModule.js
+++
@@ -1,7 +0,0 @@
-(function(){
-	
-	'use strict';
-	
-	angular.module('ranguei', ['ranguei.guest', 'ranguei.facebook']);
-	
-})();
